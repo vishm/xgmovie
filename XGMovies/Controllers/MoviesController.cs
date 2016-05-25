c9ec62dfb5053ea0997488dd51f1630fdaaded3d
Vish Murden <vishm_0@yahoo.co.uk>
2016-05-25 22:11:34 +0100

    Improve MoviesController responses, bad request now yield error message as well. Create yields url of new resource. Added MovieController unit test. Corrected/update http.formatting nupkg

--- XGMovies/Controllers/MoviesController.cs
+++ XGMovies/Controllers/MoviesController.cs
@@ -11,6 +11,7 @@ using System.Diagnostics;
 
 namespace XGMovies.Controllers
 {
+    [RoutePrefix("api/movies")]
     public class MoviesController : ApiController
     {
         static IMovieRepository _repostiory;
@@ -22,42 +23,51 @@ namespace XGMovies.Controllers
         }
 
         // GET api/movies
-        public IEnumerable<Movie> Get() => _repostiory.GetAllMovies();
-        
+        [HttpGet]
+        public IHttpActionResult Get() => Ok(_repostiory.GetAllMovies().AsEnumerable());
+
         // GET api/movies/5        
+        [HttpGet, Route("{id:int}", Name ="GetById")]
         public IHttpActionResult Get(int id)
         {
-            IHttpActionResult retValue = NotFound();
+            IHttpActionResult retValue = null;
 
             var value = _repostiory.GetMovie(id);            
             if ( value != null)
-            {                
-                retValue= Ok(value);
+            {
+                retValue = Ok(value);
             }
 
-            return retValue;
+            return retValue ?? NotFound() ;
         }
 
         // POST api/movies
+        [HttpPost]
         public IHttpActionResult Post([FromBody]Movie movie)
         {
             const int YEAR_OF_FIRST_MOVIE = 1896;
 
             if (movie == null || String.IsNullOrWhiteSpace(movie.Title) || movie.Year < YEAR_OF_FIRST_MOVIE)
             {
-                Trace.WriteLine(String.Format("Movie object failed validity checks {0}", (movie == null) ? "deserializtion failure" : ""), "POST");
-                return BadRequest();
+                var msg = String.Format("Movie object failed validity checks {0}", (movie == null) ? "deserializtion failure" : "");
+                Trace.WriteLine(msg, "POST");
+                return BadRequest(msg);
             }
 
             try
             {
+                // Return back response with Url to new content, 
                 var id = _repostiory.Store(movie);
-                return Ok(id);
+                
+                // Use CreatedAtRoute specifying a RouteName "GetById" to form Url, otherwise we
+                // end up with api/movies/Get/1 <- not "Get" is not relevant to path
+                return CreatedAtRoute<Movie>("GetById", new {  id = movie.Id }, movie);
             }
-            catch(Exception e)
+            catch (Exception e)
             {
-                Trace.WriteLine($"Unable to Store object, exception: {e.Message}", "POST");
-                return BadRequest();
+                var msg = $"Unable to Store object, due to: {e.Message}";
+                Trace.WriteLine(msg, "POST");
+                return BadRequest(msg);
             }
         }
     }
---
+++ XGMoviesTest/MoviesControllerTest.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XGMovies.Controllers;
+using System.Net.Http;
+using XGMoviesBackEnd.Models;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Web.Http.Results;
+
+namespace XGMoviesTest
+{
+    /// <summary>
+    /// Summary description for MovieControllerTest
+    /// </summary>
+    [TestClass]
+    public class MoviesControllerTest
+    {
+        public MoviesControllerTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void GetShouldReturnEmptyList()
+        {
+            // Arrange
+            var controller = new MoviesController();
+
+            // Act            
+            var response = controller.Get() as OkNegotiatedContentResult<IEnumerable<Movie>>;
+            
+            // Assert
+            Assert.IsNotNull(response, "Unexpected response");
+            Assert.IsTrue(response.Content.Count() == 0, "Expected no content");
+        }
+
+        [TestMethod]
+        public void PostInvalidNullMovie()
+        {
+            // Arrange
+            var controller = new MoviesController();
+
+            // Act            
+            var movie = new Movie() { Title = "Jungle Book" };
+
+            var response = controller.Post(null) as BadRequestErrorMessageResult;
+
+            // Assert
+            Assert.IsNotNull(response, "Unexpected response");            
+            Assert.IsTrue(response.Message.Length != 0, "Expected error message");
+        }
+
+        [TestMethod]
+        public void PostInvalidMovieState()
+        {
+            // Arrange
+            var controller = new MoviesController();
+
+            // Act            
+            var movie = new Movie() { Title = "Jungle Book" }; // missing year
+            var response = controller.Post(null) as BadRequestErrorMessageResult;
+
+            // Assert
+            Assert.IsNotNull(response, "Unexpected response");
+            Assert.IsTrue(response.Message.Length != 0, "Expected error message");
+        }
+
+        [TestMethod]
+        public void PostValidMovieState()
+        {
+            // Arrange
+            var controller = new MoviesController();
+
+            // Act            
+            var movie = new Movie() { Title = "Jungle Book", Year = 1967 };
+            var response = controller.Post(movie) as CreatedAtRouteNegotiatedContentResult<Movie>;
+
+            // Assert
+            Assert.IsNotNull(response, "Unexpected response");
+            Assert.IsTrue(response.RouteName == "GetById", "RouteName not as expectd.");        
+            Assert.IsTrue(response.Content.Id == (int)response.RouteValues["id"], "mismatch between route value");
+        }
+    }
+}
